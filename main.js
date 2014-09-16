8a609ae5e8f8c0b68c681d2b36d76fb551fda61a
Hasse Feldthaus <hafe@kb.dk>
2014-09-16 09:02:02 +0200

    Bugfix: Also supporting old IE.

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ console.log('Script initiated.');
 
 // setting up button event
 var button = document.getElementById('myButton');
-button.addEventListener('click', function(e) {
-    alert('Du trykkede på knappen');
-});
-
+if (button.attachEvent) {
+    button.attachEvent('click', function () {
+        alert('Du trykkede på knappen');
+    });
+} else {
+    button.addEventListener('click', function(e) {
+        alert('Du trykkede på knappen');
+    });
+}
