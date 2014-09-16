edac4235f375260850bb14e8113d90d56f1c03e6
Hasse Feldthaus <hafe@kb.dk>
2014-09-16 08:49:07 +0200

    Added eventlistener to button.

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 console.log('Script initiated.');
 
+// setting up button event
+var button = document.getElementById('myButton');
+button.addEventListener('click', function(e) {
+    alert('Du trykkede på knappen');
+});
+
